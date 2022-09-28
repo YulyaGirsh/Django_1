69fa4f36a934c558d5690a4bce7081a5cda478e1
123 <123@mail.ru>
2022-09-28 19:46:15 +0500

    Добавлены классы представления и формы

--- mysite/event/urls.py
+++ mysite/event/urls.py
@@ -3,12 +3,13 @@ from django.urls import path, re_path
 from .views import *
 
 urlpatterns = [
-    path('', index, name='home'),
-    path('cats/<int:catid>/', category ),
+    path('', EventsHome.as_view(), name='home'),
+    path('cats/<int:catid>/', CategoryShow.as_view() ),
     re_path(r'^archive/(?P<year>[0-9]{4})/', archive, name='year'),
     path('contact/', contact, name='contact'),
     path('login', login, name='login'),
     path('about', about, name='about'),
-    path('post/<slug:post_slug>/', post, name='post'),
-    path('area/<slug:area_slug>/', category, name='area'),
+    path('post/<slug:post_slug>/', ShowPost.as_view(), name='post'),
+    path('area/<slug:area_slug>/', CategoryShow.as_view(), name='area'),
+    path('add/', AddPost.as_view(), name='add'),
 ]
--- mysite/event/views.py
+++ mysite/event/views.py
@@ -1,48 +1,117 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse, HttpResponseNotFound, Http404
+from django.views.generic import ListView, DetailView, CreateView
+
+from .forms import *
 from .models import *
 
-menu = [{'title': 'О сайте', 'url_name': 'about'}, {'title': 'Регистрация', 'url_name': 'login'}, {'title':
-                                                                                                       'Контакты',
-                                                                                                   'url_name':
-                                                                                                       'contact'}]
+menu = [{'title': 'Главная', 'url_name': 'home'}, {'title': 'О сайте', 'url_name': 'about'}, {'title':
+                                                                                                  'Регистрация',
+                                                                                              'url_name': 'login'},
+        {'title':
+             'Добавление записи',
+         'url_name':
+             'add'}]
 
 
-def index(request):
-    post = Events.objects.all()
-    context = {'title': 'Главная страница',
-               'menu': menu, 'post': post,
-               'area_selected': 0}
+class EventsHome(ListView):
+    model = Events
+    template_name = 'event/index.html'
+    context_object_name = 'post'
 
-    return render(request, 'event/index.html', context)
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['menu'] = menu
+        context['title'] = 'Главная страница'
+        context['area_selected'] = 0
+        return context
 
-
-def area(request, area_id):
-    return HttpResponse(f'<h1>Страница категорий</h1><p>{area_id}</p>')
-
-
-def category(request, area_slug):
-    ident_area = Area.objects.get(slug=area_slug)
-    post = Events.objects.filter(cat_id=ident_area.pk)
-    context = {'title': 'Отображение по рубрикам',
-               'menu': menu,
-               'post': post,
-               'area_selected': area_slug}
-    return render(request, 'event/index.html', context=context)
+    def get_queryset(self):
+        return Events.objects.filter(is_published=True)
 
 
-def post(request, post_slug):
-    our_post = Events.objects.filter(slug=post_slug)
-    post = get_object_or_404(Events, slug=post_slug)
-    our_area = Area.objects.filter(pk=post.cat_id)
-    context = {'title': post.title,
-               'menu': menu,
-               'post': post,
-               'area_selected': post.cat_id,
-               'content': post.content,
-               'area_slug': our_area
-               }
-    return render(request, 'event/post.html', context=context)
+#     post = Events.objects.all()
+#     context = {'title': 'Главная страница',
+#                'menu': menu, 'post': post,
+#                'area_selected': 0}
+#
+#     return render(request, 'event/index.html', context)
+
+class AddPost(CreateView):
+    form_class = AddPostForm
+    template_name = 'event/add_post.html'
+    context_object_name = 'form'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['menu'] = menu
+        context['title'] = 'Добавление записи'
+        return context
+
+
+# def add_post(request, ):
+#     if request.method == 'POST':
+#         form = AddPostForm(request.POST, request.FILES)
+#         if form.is_valid():
+#             form.save()
+#             return redirect('home')
+#     else:
+#         form = AddPostForm()
+#     context = {'post': form, 'menu': menu, 'title': 'Добавление информации о новом фестивале'}
+#     return render(request, 'event/add_post.html', context=context)
+
+
+class CategoryShow(ListView):
+    model = Events
+    template_name = 'event/index.html'
+    context_object_name = 'post'
+    allow_empty = False
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['menu'] = menu
+        context['title'] = 'Локация - ' + str(context['post'][0].cat)
+        context['area_selected'] = context['post'][0].cat_id
+        return context
+
+    def get_queryset(self):
+        return Events.objects.filter(cat__slug=self.kwargs['area_slug'], is_published=True)
+
+
+# def category(request, area_slug):
+#     ident_area = Area.objects.get(slug=area_slug)
+#     post = Events.objects.filter(cat_id=ident_area.pk)
+#     context = {'title': 'Отображение по рубрикам',
+#                'menu': menu,
+#                'post': post,
+#                'area_selected': area_slug}
+#     return render(request, 'event/index.html', context=context)
+
+class ShowPost(DetailView):
+    model = Events
+    template_name = 'event/post.html'
+    slug_url_kwarg = 'post_slug'
+    context_object_name = 'post'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = context['post']
+        context['menu'] = menu
+        return context
+
+
+# def post(request, post_slug):
+#     our_post = Events.objects.filter(slug=post_slug)
+#     post = get_object_or_404(Events, slug=post_slug)
+#     our_area = Area.objects.filter(pk=post.cat_id)
+#     context = {'title': post.title,
+#                'menu': menu,
+#                'post': post,
+#                'area_selected': post.cat_id,
+#                'content': post.content,
+#                'area_slug': our_area
+#                }
+#     return render(request, 'event/post.html', context=context)
 
 
 def contact(request):
